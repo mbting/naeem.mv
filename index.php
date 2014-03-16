a7ecf1f566e4976d9dbf533ac9d385a1dac0aa94
Ibrahim Naeem <naeem@bml.com.mv>
2014-03-16 13:49:00 +0500

    basic layouts and css resets

--- index.php
+++ index.php
@@ -5,8 +5,27 @@
 	<meta name="description" content="">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Ibrahim Naeem</title>
+	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-	<p>Loading</p>
+	<div id="card">
+		<header>
+			<img src="img/pp1.jpg" alt="Ibrahim Naeem"/>
+		</header>
+		<section>
+			<h1>Ibrahim Naeem</h1>
+			<p>Hi! I'm a web developer and designer from the Maldives.</p>
+		</section>
+		<footer>
+			<nav>
+				<ul>
+					<li><a href="#">Facebook</a></li>
+					<li><a href="#">Google+</a></li>
+					<li><a href="#">Twitter</a></li>
+					<li><a href="#">Instagram</a></li>
+				</ul>
+			</nav>
+		</footer>
+	</div>
 </body>
 </html>
